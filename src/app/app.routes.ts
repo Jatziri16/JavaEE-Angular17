7737740e7630aa6cc7383916708b220d6a6ce015
Jatziri16 <fatimajatziri@hotmail.com>
2023-12-22 19:54:58 -0600

    Se crea componente 'videojuegos'
    Se configura el proyecto para que la vista principal sea el componente de videojuegos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { VideojuegosComponent } from './components/videojuegos/videojuegos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'videojuegos'
+    },
+    {
+        path: 'videojuegos',
+        component: VideojuegosComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'videojuegos'
+    },
+];
